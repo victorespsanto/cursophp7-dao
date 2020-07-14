ea129685a7dbb15a63ecb82512b6cd9b14b68d6e
Victor Ribeiro <victorespsanto@gmail.com>
2020-07-14 16:32:09 -0300

    iniciamos o projeto

---
+++ class/sql.php
@@ -0,0 +1,69 @@
+<?php 
+
+
+// criação da classe Sql extendida da Classe PDO (recebe todos os métodos de PDO)
+
+class Sql extends PDO {
+
+    private $conn;
+
+    // criação do método construtor para conexão ao banco de dados...
+
+    public function __construct() {
+
+        $this->conn = new PDO("mysql:dbname=dbphp7;host=localhost", "root", "");
+
+    }
+
+
+    // criação do método para inclusão de um parãmetro
+
+    public function setParam($statement, $key, $value) {
+
+            $$statement->bindParam($key, $value);  // encaixando os parâmetros no comando
+
+    }
+
+
+    // criação do método para inclusão de vários  parãmetroa
+
+    public function setParams($statement, $parameters = array()) {
+
+        foreach ($parameters as $key => $value) {  // percorre todos os parâmetros dado como array
+            $$statement->setParam($key, $value);   // chamando o método setParam() 
+        }
+
+
+    }
+
+
+    // criação do método de pesquisa
+
+    public function query($rawQuery, $params = array()) {
+
+        $stmt = $this->conn->prepare($rawQuery);
+        
+        $this->setParams($stmt, $params);
+
+        $stmt->execute();
+
+        return $stmt;
+
+        
+    }
+
+    public function select($rawQuery, $params = array()):array {
+
+       $stmt = $this->query($rawQuery, $params);
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+
+
+}
+
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php 
+
+require_once("config.php");
+
+$sql = new Sql();
+
+$usuario = $sql->select("SELECT * FROM tb_usuarios");
+
+echo  json_encode($usuario);
+
+ ?>
