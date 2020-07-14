6df9d28067a69c4a105593bafcfaed258f2a59f9
Victor Ribeiro <victorespsanto@gmail.com>
2020-07-14 20:02:49 -0300

    atualização do projeto

--- class/usuario.php
+++ class/usuario.php
@@ -3,10 +3,17 @@
 
 class Usuario {
 
+	// atributos da classe, campos da tabela do banco de dados
+
 	private $idusuario;
 	private $deslogin;
 	private $dessenha;
 	private $dtcadastro;
+
+	// fim da declaração dos atributos da classe
+
+
+	// métodos getters e setters para manipulação dos atributos da classe
 
 	public function setIdusuario($value) {
 		$this->idusuario = $value;
@@ -40,6 +47,10 @@ class Usuario {
 		return $this->dtcadastro;
 	}
 
+	// fim da declaração dos métodos getters e setters de manipulação dos atributos da classe
+
+	// funcão para carregar os atributos da classe
+
 	public function loadById($id) {
 
 		$sql = new Sql();
@@ -58,6 +69,10 @@ class Usuario {
 
 	}
 
+	// fim do método
+
+	// função para retornar todos os registros do banco de dados
+
 	public static function getLista() {
 
 		$sql = new Sql();
@@ -65,57 +80,68 @@ class Usuario {
 		return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
 	}
 
+	// fim
+
+	// função para retornar pesquisa de usuários do banco de dados
+
 	public static function search($login) {
 
 		$sql = new Sql();
 
-		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(  // tag LIKE permite pesquisa de parte de registro do usuario. Uso de "%%"
 			":SEARCH"=>"%" . $login . "%"
 		));
 
 	}
 
-	public function login($login, $password) {
+	// fim
+
+	// função para autenticação de usuario pelo login e senha. Retorna erro se um dos valores estiver errado   
+
+	public function login($login, $password) {  
 
 		$sql = new Sql();
 
-		$result = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :SENHA", array(
+		$result = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :SENHA", array(   
 
 			":LOGIN"=>$login,
 			":SENHA"=>$password
 		));
 
-		if ($result[0]) {
+		if ($result[0]) {  // se o array tiver pelo menos um componente retorna TRUE
 
 			$row = $result[0];
 
 			$this->setIdusuario($row['idusuario']);
 			$this->setDeslogin($row['deslogin']);
 			$this->setDessenha($row['dessenha']);
-			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+			$this->setDtcadastro(new DateTime($row['dtcadastro']));  // DateTime() para formatação de data
 
 		} else {
 
-			throw new Exception("Login e/ou senha inválidos!");
+			throw new Exception("Login e/ou senha inválidos!");   // retorna o erro caso for informado login e/ou senha errados.
 			
 		}
 
 	}
 
+	//fim
+
+	// funão toString para decriçao do objeto
+
 	public function __toString() {
 
-		return json_encode(array(
+		return json_encode(array(   // retorna um array formatado em json.
 
 			'idusuario'=>$this->getIdusuario(),
 			'deslogin'=>$this->getDeslogin(),
 			'dessenha'=>$this->getDessenha(),
-			'dtcadastro'=>$this->getDtcadastro()->format('d/m/Y H:i:s')
+			'dtcadastro'=>$this->getDtcadastro()->format('d/m/Y H:i:s')   // formatação de data e hora.
 
 		));
 	}
 
-
-
+	// fim da função toString.
 
 }
 
